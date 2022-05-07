fa673ed4148ffd7ee0334dfc4b416563b83f3632
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-05-07 14:58:47 +0500

    Add Score board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./components/Card";
 import Grid from "./components/Grid";
+import ScoreCard from "./components/ScoreCard";
 import test from "./test.jpeg";
 import index from "./index.jpeg";
 
@@ -9,6 +10,12 @@ const App = () => {
 
   return (
     <div>
+
+      <div className="score-board">
+        <ScoreCard><p className="score-card-text">Score:</p> </ScoreCard>
+        <ScoreCard><p className="score-card-text">Best Score:</p> </ScoreCard>
+      </div>
+
       <Grid>
         {images.map((item, index) => {
           return (
---
+++ src/components/ScoreCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScoreCard = (props) => {
+  return <div className="score-card">{props.children}</div>;
+};
+
+export default ScoreCard;
